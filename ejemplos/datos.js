739e4cbf6b175702eb5adb623673a2ca2d064621
Antonio Rueda Treviño <antonio.rueda@gmail.com>
2018-04-20 11:31:15 +0200

    Create datos.js

---
+++ ejemplos/datos.js
@@ -0,0 +1,18 @@
+{
+   "alumnos": [
+      {
+         "id":"01",
+         "alumno": "Antonio Rueda",
+         "curso": "segundo",
+         "rama": "formacion profesional",
+         "periodo":"2017/2018"
+         },
+         {
+         "id":"02",
+         "alumno": "Steve Jobs",
+         "curso": "primero",
+         "rama": "universidad",
+         "periodo":"1978/1979"
+         }
+   ]
+}
